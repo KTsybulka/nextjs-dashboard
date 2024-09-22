1085031ff9d2c3928b99d17c1f32dd0fba5891fb
KTsybulka <ktsybulka11@gmail.com>
2024-09-22 08:53:14 -0400

    first commit labW03

---
+++ app/fonts.ts
@@ -0,0 +1,10 @@
+// We'll use this file to manage the fonts for your whole app.
+
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'],
+ });
