4e3eccae7f24f6016a190c41206621d896f00943
KTsybulka <ktsybulka11@gmail.com>
2024-09-30 08:53:17 -0400

    Add Chapter # 11

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en">S
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
